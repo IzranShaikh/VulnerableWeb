62459338cd36d6d592812e5f66477fef409f6c93
IzranShaikh <35726516+IzranShaikh@users.noreply.github.com>
2018-12-27 21:18:48 +0530

    Add files via upload
    Tinymce - WYSIWYG Text Editor

---
+++ tinymce/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Test of WYSIWYG</title>
+	<script type="text/javascript" src="tinymce.min.js"></script>
+	<script>
+		tinymce.init({ selector:'textarea' });
+	</script>
+</head>
+<body>
+	<textarea>Not Working</textarea>
+</body>
+</html>
